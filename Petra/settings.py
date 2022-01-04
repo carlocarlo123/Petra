600c96065876d94eabc14f8b7b9420cad270d4b3
Ali <ali.abbass@lau.edu>
2022-01-04 21:39:52 +0200

    twiked the App

--- Petra/settings.py
+++ Petra/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Petra.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"TEMPLATES")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
